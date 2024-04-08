8c6cebd2d843557a19ffdd666b286eb1fdc86c10
Catelyn Jones <cxj15@pct.edu>
2024-04-08 15:14:35 -0400

    Added more droppers, still troubleshooting Z position issues

--- Assets/Scripts/DropperCollision.cs
+++ Assets/Scripts/DropperCollision.cs
@@ -5,64 +5,87 @@ using UnityEngine;
 
 public class DropperCollision : MonoBehaviour
 {
-
     private Rigidbody dropperRB;
+
     public static DropperCollision instance;
-   
+    //need to change these to colors or items later bc this is confusing
     public GameObject dropper;
     public GameObject bigDropper;
+    public GameObject extrabigDropper;
+    public GameObject hugeDropper;
     public GameObject smallDropper;
     public bool isActiveDropper = true;
+
     private void Start()
     {
         dropperRB = GetComponent<Rigidbody>();
     }
     private void OnCollisionEnter(Collision collision)
     {
-
+        //check if active dropper
         if (!isActiveDropper)
             return; 
 
+        //if dropper collides with boundary...
         if (collision.gameObject.CompareTag("boundary"))
         {
-            Debug.Log(gameObject.name);
+            //Debug.Log(gameObject.name);
+
+            //adjusting velocity to limit random movement
             dropperRB.velocity = Vector3.zero;
             dropperRB.angularVelocity = Vector3.zero;
+            
+
             isActiveDropper = false;
 
         }
 
-        if (isActiveDropper)
+        if (isActiveDropper) //if active -- hasn't collided with boundary yet
         {
-            if (collision.gameObject.name == gameObject.name && gameObject.name == "Dropper(Clone)")
+            if (collision.gameObject.name == gameObject.name && gameObject.name == "ExtraBigDropper(Clone)") //two "ExtraBigDropper" collide
             {
                 Debug.Log("Dropper Collision");
-                Merge(bigDropper);
-                GameManager.instance.UpdateScore(50);
-                Destroy(collision.gameObject);
+                Merge(hugeDropper); //make hugedropper
+                GameManager.instance.UpdateScore(100); //update score
+                Destroy(collision.gameObject); //destroy dropper
             }
-            else if (collision.gameObject.name == gameObject.name && gameObject.name == "SmallDropper(Clone)")
+            else if (collision.gameObject.name == gameObject.name && gameObject.name == "BigDropper(Clone)") //two "BigDropper" collide
+            {
+                Debug.Log("Dropper Collision");
+                Merge(extrabigDropper); //make extrabigdropper
+                GameManager.instance.UpdateScore(100); //update score
+                Destroy(collision.gameObject); //destroy dropper
+            }
+            else if (collision.gameObject.name == gameObject.name && gameObject.name == "MediumDropper(Clone)") //two "MediumDropper" collide
+            {
+                Debug.Log("Dropper Collision");
+                Merge(bigDropper); //make bigdropper
+                GameManager.instance.UpdateScore(50); //update score
+                Destroy(collision.gameObject); //destroy dropper
+            }
+            else if (collision.gameObject.name == gameObject.name && gameObject.name == "SmallDropper(Clone)") //two "SmallDropper" collide
             {
                 Debug.Log("Small Dropper Collision");
-                Merge(dropper);
-                GameManager.instance.UpdateScore(25);
-                Destroy(collision.gameObject);
+                Merge(dropper); //make medium dropper
+                GameManager.instance.UpdateScore(25); //update score
+                Destroy(collision.gameObject); //destroy dropper
             }
         }
 
     }
+    
 
     private void Merge(GameObject mergedDropperPrefab)
     {
         Debug.Log("Merge.");
 
-        Vector3 mergeOffset = new Vector3(0.5f, 0.5f, 0);
+        Vector3 mergeOffset = new Vector3(0.5f, 0.5f, 0); //offset to avoid collisions on merge
         Vector3 newPosition = transform.position + mergeOffset;
         GameObject mergedDropper = Instantiate(mergedDropperPrefab, newPosition, Quaternion.identity);
 
         mergedDropper.GetComponent<DropperCollision>().isActiveDropper = false;
 
-        Destroy(gameObject);
+        Destroy(gameObject); //destroy other dropper
 
     }
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,6 +19,8 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+
+        //set score on start
         score = 0;
     }
 
